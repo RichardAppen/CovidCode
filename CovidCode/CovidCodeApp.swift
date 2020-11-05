2c76e169dd8a6b49f3a7814057f373f9f0531715
Richie Appen <rappen@ucsd.edu>
2020-11-04 20:22:10 -0800

    Initial Commit

---
+++ CovidCode/CovidCodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  CovidCodeApp.swift
+//  CovidCode
+//
+//  Created by Richard Appen on 11/4/20.
+//
+
+import SwiftUI
+
+@main
+struct CovidCodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
